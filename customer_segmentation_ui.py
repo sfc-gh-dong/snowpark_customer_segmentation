f9f5a26a293db730c1517758b1fcdf83bed6fba7
Daniel Ong <111833269+sfc-gh-dong@users.noreply.github.com>
2023-01-31 22:26:26 +0800

    Update customer_segmentation_ui.py

--- customer_segmentation_ui.py
+++ customer_segmentation_ui.py
@@ -1,5 +1,3 @@
-# streamlit_app.py
-
 import streamlit as st
 import snowflake.connector
 from snowflake.snowpark.session import Session
@@ -7,18 +5,11 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-# Snowflake connection info is saved in config.py
-# from config import snowflake_conn_prop
 
 # Initialize connection.
-# Uses st.experimental_singleton to only run once.
-# session = Session.builder.configs(snowflake_conn_prop).create()
-
 session = Session.builder.configs(st.secrets["snowflake"]).create()
 
 # Perform query.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-
 st.header('Customer Segmentation example with Snowpark using K-Means')
 
 st.write('Sample cluster data that shows the recency, frequency and monetary attributes of each customer. Data live from Snowflake')
