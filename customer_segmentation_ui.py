8642d4d8a787605f14ed42184d59c993ad380986
Daniel Ong <111833269+sfc-gh-dong@users.noreply.github.com>
2023-02-01 23:31:12 +0800

    Update customer_segmentation_ui.py

--- customer_segmentation_ui.py
+++ customer_segmentation_ui.py
@@ -56,7 +56,7 @@ st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 st.markdown("**:red[Cluster 3]** are your Loyalists. They generally spend more money and more frequently.")
 st.markdown("**:blue[Cluster 1]** are your Growing customers. They spend less money and less frequently, but they spent in the last 5 months.")
-st.markdown("**:orange[Cluster 2]** are your Churning customers. They spend less money and less frequently, but they spent beyond the last 5 months.")
+st.markdown("**:orange[Cluster 2]** are your Churning customers. They spend less money and less frequently, and they spent beyond the last 5 months.")
 st.markdown("**Cluster** **0** sit somewhere in between.")
 
 
