993eda8c262fb132fb8e6cb756f0a5c56539522c
Daniel Ong <111833269+sfc-gh-dong@users.noreply.github.com>
2023-02-06 14:43:04 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import snowflake.connector
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * RFM_Clusters LIMIT 10;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
