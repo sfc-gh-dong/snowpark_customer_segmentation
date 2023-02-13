30d537c111d63e24aba4d5d2f71128a72d53f82f
Daniel Ong <daniel.ong@snowflake.com>
2023-02-13 14:05:16 +0800

    x

--- customer_segmentation_ui.py
+++ customer_segmentation_ui.py
@@ -1,3 +1,5 @@
+# streamlit_app.py
+
 import streamlit as st
 import snowflake.connector
 from snowflake.snowpark.session import Session
@@ -5,62 +7,74 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
+# Snowflake connection info is saved in config.py
+# from config import snowflake_conn_prop
 
 # Initialize connection.
+# Uses st.experimental_singleton to only run once.
+# session = Session.builder.configs(snowflake_conn_prop).create()
+
 session = Session.builder.configs(st.secrets["snowflake"]).create()
 
 # Perform query.
-st.header('Customer Segmentation example with Snowpark using K-Means')
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 
-st.write('Sample cluster data that shows the recency, frequency and monetary attributes of each customer. Data live from Snowflake')
-df = session.table("RFM_Clusters")
-df_pd = df.to_pandas()
-st.dataframe(df_pd)
-df_pd["Cluster"] = df_pd["Cluster"].astype(str)
+st.header('Customer Segmentation example with Snowpark using K-Means. Data is queried live from Snowflake')
 
-st.subheader('Frequency vs Recency')
+st.write('Sample cluster data that shows the recency, frequency and monetary attributes of each customer')
 
-fig = px.scatter(
-    df_pd,
-    x="FREQUENCY",
-    y="RECENCY",
-    color="Cluster",
-    opacity=0.5
-)
+def get_data():
+    df = session.table("RFM_Clusters")
+    df_pd = df.to_pandas()
+    st.dataframe(df_pd)
+    df_pd["Cluster"] = df_pd["Cluster"].astype(str)
+    return df_pd
 
-st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+def main:
 
-st.subheader('Frequency vs Monetary')
+    st.subheader('Frequency vs Recency')
 
-fig = px.scatter(
-    df_pd,
-    x="FREQUENCY",
-    y="MONETARY",
-    color="Cluster",
-    opacity=0.5
-)
+    df_pd = get_data()
 
-st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+    fig = px.scatter(
+        df_pd,
+        x="FREQUENCY",
+        y="RECENCY",
+        color="Cluster",
+        opacity=0.5
+    )
 
-st.subheader('Recency vs Monetary')
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
-fig = px.scatter(
-    df_pd,
-    x="RECENCY",
-    y="MONETARY",
-    color="Cluster",
-    opacity=0.5
-)
+    st.subheader('Frequency vs Monetary')
 
-st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+    fig = px.scatter(
+        df_pd,
+        x="FREQUENCY",
+        y="MONETARY",
+        color="Cluster",
+        opacity=0.5
+    )
 
-st.markdown("**:red[Cluster 3]** are your Loyalists. They generally spend more money and more frequently.")
-st.markdown("**:blue[Cluster 1]** are your Growing customers. They spend less money and less frequently, but they spent in the last 5 months.")
-st.markdown("**:orange[Cluster 2]** are your Churning customers. They spend less money and less frequently, and they spent beyond the last 5 months.")
-st.markdown("**Cluster** **0** sit somewhere in between.")
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+    st.subheader('Recency vs Monetary')
+
+    fig = px.scatter(
+        df_pd,
+        x="RECENCY",
+        y="MONETARY",
+        color="Cluster",
+        opacity=0.5
+    )
+
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+    st.markdown("**:red[Cluster 2]** are your Loyalists. They generally spend more money and more frequently.")
+    st.markdown("**:blue[Cluster 1]** spend less money and less frequently, but they spent in the last 5 months.")
+    st.markdown("**:orange[Cluster 3]** spend less money and less frequently, but they spent beyond the last 5 months.")
+    st.markdown("**Cluster** **0** sit somewhere in between.")
 
 
-
-
-
-
+if __name__ == "__main__":
+    main()
