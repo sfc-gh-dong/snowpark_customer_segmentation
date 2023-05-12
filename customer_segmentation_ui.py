169eaba5f7b7018e08a5019d08b7190db3513b89
Daniel Ong <111833269+sfc-gh-dong@users.noreply.github.com>
2023-05-12 11:58:25 +0800

    Update customer_segmentation_ui.py

--- customer_segmentation_ui.py
+++ customer_segmentation_ui.py
@@ -23,6 +23,12 @@ st.header('Customer Segmentation example with Snowpark using K-Means. Data is qu
 
 st.write('Sample cluster data that shows the recency, frequency and monetary attributes of each customer')
 
+df = session.table("RFM_Clusters")
+df_pd = df.to_pandas()
+st.dataframe(df_pd)
+
+
+
 
 
 
