74c8205748e7b1c494c9f2be736c0ae25f317c43
Daniel Ong <111833269+sfc-gh-dong@users.noreply.github.com>
2023-05-22 13:13:54 +0800

    Update customer_segmentation_ui.py

--- customer_segmentation_ui.py
+++ customer_segmentation_ui.py
@@ -13,48 +13,59 @@ session = Session.builder.configs(st.secrets["snowflake"]).create()
 st.header('Customer Segmentation example with Snowpark using K-Means')
 
 st.write('Sample cluster data that shows the recency, frequency and monetary attributes of each customer')
-df = session.table("RFM_Clusters")
-df_pd = df.to_pandas()
-st.dataframe(df_pd)
-df_pd["Cluster"] = df_pd["Cluster"].astype(str)
+
+@st.cache_data
+def get_data():
+    df = session.table("RFM_Clusters")
+    df_pd = df.to_pandas()
+    st.dataframe(df_pd)
+    df_pd["Cluster"] = df_pd["Cluster"].astype(str)
+    return df_pd
 
 st.subheader('Frequency vs Recency')
 
-fig = px.scatter(
-    df_pd,
-    x="FREQUENCY",
-    y="RECENCY",
-    color="Cluster",
-    opacity=0.5
-)
+def main():
+    
+    df_pd = get_data()
 
-st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+    fig = px.scatter(
+        df_pd,
+        x="FREQUENCY",
+        y="RECENCY",
+        color="Cluster",
+        opacity=0.5
+    )
 
-st.subheader('Frequency vs Monetary')
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
-fig = px.scatter(
-    df_pd,
-    x="FREQUENCY",
-    y="MONETARY",
-    color="Cluster",
-    opacity=0.5
-)
+    st.subheader('Frequency vs Monetary')
 
-st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+    fig = px.scatter(
+        df_pd,
+        x="FREQUENCY",
+        y="MONETARY",
+        color="Cluster",
+        opacity=0.5
+    )
 
-st.subheader('Recency vs Monetary')
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
-fig = px.scatter(
-    df_pd,
-    x="RECENCY",
-    y="MONETARY",
-    color="Cluster",
-    opacity=0.5
-)
+    st.subheader('Recency vs Monetary')
 
-st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+    fig = px.scatter(
+        df_pd,
+        x="RECENCY",
+        y="MONETARY",
+        color="Cluster",
+        opacity=0.5
+    )
 
-st.markdown("**:red[Cluster 3]** are your Loyalists. They generally spend more money and more frequently.")
-st.markdown("**:blue[Cluster 1]** are your Growing customers. They spend less money and less frequently, but they spent in the last 5 months.")
-st.markdown("**:orange[Cluster 2]** are your Churning customers. They spend less money and less frequently, and they spent beyond the last 5 months.")
-st.markdown("**Cluster** **0** sit somewhere in between.")
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+    st.markdown("**:red[Cluster 3]** are your Loyalists. They generally spend more money and more frequently.")
+    st.markdown("**:blue[Cluster 1]** are your Growing customers. They spend less money and less frequently, but they spent in the last 5 months.")
+    st.markdown("**:orange[Cluster 2]** are your Churning customers. They spend less money and less frequently, and they spent beyond the last 5 months.")
+    st.markdown("**Cluster** **0** sit somewhere in between.") 
+
+if __name__ == "__main__":
+    main()
